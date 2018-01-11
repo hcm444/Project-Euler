e287ac9f05bf79a7b595722e4b81a208a93852c2
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-01-10 22:33:24 -0800

    Update LargePrime.java

--- LargePrime.java
+++ LargePrime.java
@@ -6,13 +6,17 @@ public class LargePrime {
 		for (long i = 2; i < input; i++) { //long because int will divide by zero
 			if (Factor(input, i) && Prime(input / i))
 				System.out.println(input / i);
+				//Output for 600851475143:
+				//6857
+				//1471
+				//839
+				//71
 		}
 	}
 	public static boolean Factor(long x, long y) {
 		if (x % y == 0)
 			return true;
 		return false;
-		
 	}
 	public static boolean Prime(long x) {
 		for (int i = 2; Math.pow(i, 2) < x; i++) {
