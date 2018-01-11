af71c6b7b9e3af1e7879a6c6c2e2ca2b4f799765
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-01-10 22:31:55 -0800

    Problem 3
    What is the largest prime factor of the number 600851475143?

---
+++ LargePrime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class LargePrime {
+	static Scanner ss = new Scanner(System.in);
+	public static void main(String[] args) {
+		long input = ss.nextLong();
+		for (long i = 2; i < input; i++) { //long because int will divide by zero
+			if (Factor(input, i) && Prime(input / i))
+				System.out.println(input / i);
+		}
+	}
+	public static boolean Factor(long x, long y) {
+		if (x % y == 0)
+			return true;
+		return false;
+		
+	}
+	public static boolean Prime(long x) {
+		for (int i = 2; Math.pow(i, 2) < x; i++) {
+			if (x % i == 0)
+				return false;
+		}
+		return true;
+	}
+}
