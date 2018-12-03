0d1f7111052cb809f7bfa864a2392216713beca9
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-12-02 21:01:08 -0800

    Update Problem5.cpp

--- Problem5.cpp
+++ Problem5.cpp
@@ -1,37 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <iostream>
-using namespace std;
-int main() {
- 	int i = 20;
-			while (true)
-			{
-				if ((i % 1 == 0) &&
-					(i % 2 == 0) &&
-					(i % 3 == 0) &&
-					(i % 4 == 0) &&
-					(i % 5 == 0) &&
-					(i % 6 == 0) &&
-					(i % 7 == 0) &&
-					(i % 8 == 0) &&
-					(i % 9 == 0) &&
-					(i % 10 == 0) &&
-					(i % 11 == 0) &&
-					(i % 12 == 0) &&
-					(i % 13 == 0) &&
-					(i % 14 == 0) &&
-					(i % 15 == 0) &&
-					(i % 16 == 0) &&
-					(i % 17 == 0) &&
-					(i % 18 == 0) &&
-					(i % 19 == 0) &&
-					(i % 20 == 0) )
-				{
-					break;
-				}
-				i += 20;
-			}
-			cout << i;
-		}
-
+#include<iostream>
+int main(){size_t x=10;size_t y=11;while(y<20)
+{if((x%y)==0)
+y++;else{x++;y=11;}}
+std::cout<<x;}
