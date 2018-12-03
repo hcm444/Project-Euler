520c40c7e34a897fcae3d41020559778da5d9ef5
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-12-03 02:37:22 -0800

    Update Problem10.cpp

--- Problem10.cpp
+++ Problem10.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
-using namespace std;
-int sum;
-bool isPrime(int n){
-    if(n < 2) return false;
-    if(n == 2) return true;
-    if(n % 2 == 0) return false;
-    for(int i=3; (i*i)<=n; i+=2){
-        if(n % i == 0 ) return false;
-    }
-    return true;
+bool isPrime(int n) {
+  if ((n < 2) || (n % 2 == 0)) return false;
+  for (int i = 3;
+    (i * i) <= n; i += 2) {
+    if (n % i == 0) return false;
+  }
+  if (n == 2) return true;
 }
-int main()
-{
-	unsigned long long sum = 0;
-	for (int i = 3; i < 2000000; i += 2)
-	{
-		if (isPrime(i))
-		{
-			sum += i;
-		}
-	}
-	cout << sum + 2;
-	return 0;
+int main() {
+  unsigned long long sum = 0;
+  for (int i = 3; i < 2000000; i += 2) {
+    if (isPrime(i)) {
+
+      sum += i;
+    }
+  }
+  std::cout << sum + 2;
+  return 0;
 }
