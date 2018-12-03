d6cff8fb6c22b623a7ceb44d4d5a7f347673ec7e
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-12-02 22:11:24 -0800

    Update Problem1.cpp
    Made it much faster and more optimized

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,22 +1,12 @@
-#include <stdio.h>
 #include <iostream>
-using namespace std;
-int main(void)
-
-{
-  int sum1 = 0, sum2 = 0, sum3 = 0, i, answer;
-  for (i = 0; i < 1000; i++) {
-    if (i % 3 == 0) {
-      sum1 += i;
+  int main(void) {
+    int sum = 0, i, answer;
+    for (i = 0; i < 1000; i++) {
+      if ((i % 3 == 0) || (i % 5 == 0) || (i % 15 == 0)) {
+        sum += i;
+      }
     }
-    if (i % 5 == 0) {
-      sum2 += i;
-    }
-    if (i % 15 == 0) {
-      sum3 += i;
-    }
+    answer = sum;
+    std::cout << answer;
+    return 0;
   }
-  answer = sum1 + sum2 - sum3;
-  cout << answer << endl;
-  return 0;
-}
