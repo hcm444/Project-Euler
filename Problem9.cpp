583d987774d25bc9cb2a755cf7774f1a4284a082
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-12-03 01:45:04 -0800

    Create Problem9.cpp

---
+++ Problem9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+  int main() {
+    int a, b, c, x = 1000; //one Pythagorean triplet for which a + b + c = 1000.
+    for (a = 1; a < x; a++) { //a < b < c
+      for (b = a; b < x / 2; b++) {
+        c = x - a - b;
+        if (a * a + b * b == c * c) { //a^2+b^2=c^2
+          std::cout << a * b * c; //multiply em
+        }
+      }
+    }
+  }
