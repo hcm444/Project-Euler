801611afbdaac8bb895aad075f362a92b259f03b
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-12-02 22:56:40 -0800

    Update Problem1.cpp

--- Problem1.cpp
+++ Problem1.cpp
@@ -2,7 +2,7 @@
   int main(void) {
     int sum = 0, i, answer;
     for (i = 0; i < 1000; i++) {
-      if ((i % 3 == 0) || (i % 5 == 0) || (i % 15 == 0)) {
+      if ((i % 3 == 0) || (i % 5 == 0)) {
         sum += i;
       }
     }
@@ -10,9 +10,3 @@
     std::cout << answer;
     return 0;
   }
-
-/*
-233168
-Process returned 0 (0x0)   execution time : 0.012 s
-Press any key to continue.
-*/
