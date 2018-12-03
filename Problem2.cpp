7c7394728743c8bf31a0ce27cd88319a19f6ef44
Henry Meier <32826270+hcm444@users.noreply.github.com>
2018-12-03 01:22:11 -0800

    Update Problem2.cpp

--- Problem2.cpp
+++ Problem2.cpp
@@ -1,6 +1,12 @@
-#include<iostream>
-int main()
-{int s=0,x=1,y=x,z=2;while(z<4000000)
-{z=x+y;s+=z*!(z%2);x=y;y=z;}
-std::cout<<s;return 0;}
-
+#include <iostream>
+  int main() {
+    int s = 0, x = 1, y = x, z = 2;
+    while (z < 4000000) {
+      z = x + y;
+      s += z * !(z % 2);
+      x = y;
+      y = z;
+    }
+    std::cout << s;
+    return 0;
+  }
