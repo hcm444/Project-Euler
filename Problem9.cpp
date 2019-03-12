a927384d19bb0600dea39f5eb6a577bc44ca6926
Henry Meier <32826270+hcm444@users.noreply.github.com>
2019-03-12 13:29:50 -0700

    Update Problem9.cpp
    commenting

--- Problem9.cpp
+++ Problem9.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-  int main() {
+  int main() { //simple func
     int a, b, c, x = 1000; //one Pythagorean triplet for which a + b + c = 1000.
     for (a = 1; a < x; a++) { //a < b < c
       for (b = a; b < x / 2; b++) {
-        c = x - a - b;
+        c = x - a - b; //c is x minus a minus b
         if (a * a + b * b == c * c) { //a^2+b^2=c^2
           std::cout << a * b * c; //multiply em
         }
