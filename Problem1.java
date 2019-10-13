1464722bb2a6aa3cdfc4f89d93fee6d191e8d74e
Henry Meier <32826270+hcm444@users.noreply.github.com>
2019-10-13 16:01:46 -0700

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,17 +1,16 @@
 public class Multiples {
-    public static void main(String[] args) {
-    int sum = 0;
-        for (int i = 1; i < 1000; i++) {
-            if ((i % 3) == 0){
-                System.out.println(i + " ");
-                sum += i;
-            }
-            else if ((i % 5) == 0){
-                System.out.println(i + " ");
-                sum += i;
-                }
-            }
-           System.out.println("Sum of numbers : " + sum);
-        }
+ public static void main(String[] args) {
+  int sum = 0, c = 1;
+  if (c < 1000) {
+   do {
+    if ((c % 3) == 0 || (c % 5) == 0) {
+     System.out.println(c + " ");
+     sum += c;
     }
+    c++;
+   } while (c < 1000);
+  }
+  System.out.println("Sum of numbers : " + sum);
+ }
+}
 //https://github.com/hcm444
