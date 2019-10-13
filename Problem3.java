8c3122374ecde6e5e5e6a6fc51e6b83270179a0e
Henry Meier <32826270+hcm444@users.noreply.github.com>
2019-10-13 16:44:16 -0700

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 public class LargePrime {
-	static Scanner ss = new Scanner(System.in);
-	public static void main(String[] args) {
-		long input = ss.nextLong();
-		for (long i = 2; i < input; i++) { //long because int will divide by zero
-			if (Factor(input, i) && Prime(input / i))
-				System.out.println(input / i);
-				//Output for 600851475143:
-				//6857
-				//1471
-				//839
-				//71
-		}
-	}
-	public static boolean Factor(long x, long y) {
-		if (x % y == 0)
-			return true;
-		return false;
-	}
-	public static boolean Prime(long x) {
-		for (int i = 2; Math.pow(i, 2) < x; i++) {
-			if (x % i == 0)
-				return false;
-		}
-		return true;
-	}
+ private static Scanner ss = new Scanner(System.in);
+ public static void main(String[] args) {
+  long input = ss.nextLong();
+  long i = 2;
+  while (i < input) { //long because int will divide by zero
+   if (Factor(input, i) && Prime(input / i)) {
+    System.out.println(input / i);
+   }
+   //Output for 600851475143:
+   //6857
+   //1471
+   //839
+   //71
+   i++;
+  }
+ }
+ private static boolean Factor(long x, long y) {
+  return x % y == 0;
+ }
+ private static boolean Prime(long x) {
+  return IntStream.iterate(2, i -> Math.pow(i, 2) < x, i -> i + 1).noneMatch(i -> x % i == 0);
+ }
 }
