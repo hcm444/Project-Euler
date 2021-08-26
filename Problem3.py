ca27da06c273d7678ff661712cf6cf87a8c82ebc
Henry Meier <32826270+hcm444@users.noreply.github.com>
2021-08-26 11:27:08 -0700

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -1,14 +1,14 @@
-import math
-divisible = []
-factors = []
-i = 2
-input = 600851475143
-while i < input:
-    while input % i == 0:
-        input = input / i
-        divisible.append(input)
-        factors.append(i)
-    i = i + 1
-last_elem = divisible[-1]
-factors.append(int(last_elem))
-print (factors)
+import math #import math library
+divisible = [] #dvisiible list for debugging
+factors = [] #list for prime factors
+i = 2 #first prime
+input = 600851475143 #input goes here
+while i < input: #less then input
+    while input % i == 0: #check even
+        input = input / i #divide by 2 until odd
+        divisible.append(input) #add inputs
+        factors.append(i) #add factors
+    i = i + 1 #odd at this point
+last_elem = divisible[-1] #steal last value of div list
+factors.append(int(last_elem)) #convert to integer and append
+print (factors) #final prime factor list
